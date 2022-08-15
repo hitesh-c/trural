31f9e078c518cb2f2255122f6ae67b5687d1eb5c
Hitesh Chawla <hiteshchawla50@yahoo.com>
2022-08-15 23:22:21 +0530

    Updates

---
+++ precache-manifest.92171f16d7cd6db776b29a12bb5cfb1a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c064e48467913baae5151997ca74d1d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5ff4a836a9cb2b83c4a",
+    "url": "/static/css/2.cf1ed878.chunk.css"
+  },
+  {
+    "revision": "d5cedae3b4fd5df4cd17",
+    "url": "/static/css/main.596184c9.chunk.css"
+  },
+  {
+    "revision": "c5ff4a836a9cb2b83c4a",
+    "url": "/static/js/2.0327f284.chunk.js"
+  },
+  {
+    "revision": "fdc4023fa9d7d62afa8f74ec645ba6c4",
+    "url": "/static/js/2.0327f284.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5cedae3b4fd5df4cd17",
+    "url": "/static/js/main.cbe64230.chunk.js"
+  },
+  {
+    "revision": "548616e762ee544bf5a8",
+    "url": "/static/js/runtime-main.42543a3e.js"
+  },
+  {
+    "revision": "145bf67bc59049251bfe996142c1a39c",
+    "url": "/static/media/g.145bf67b.svg"
+  },
+  {
+    "revision": "f752069aa979db591dd419ed9dcea40b",
+    "url": "/static/media/str.f752069a.svg"
+  },
+  {
+    "revision": "442fa2ec8c0ac7f2e0db0fdde668e6c1",
+    "url": "/static/media/trural.442fa2ec.png"
+  }
+]);
