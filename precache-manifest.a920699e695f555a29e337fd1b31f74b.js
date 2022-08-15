14162698f3d0771b18e37f8c1b646f3ed96cd2a2
Hitesh Chawla <hiteshchawla50@yahoo.com>
2022-08-15 23:27:32 +0530

    Updates

---
+++ precache-manifest.a920699e695f555a29e337fd1b31f74b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69010a6daaa24c5dee3a396c5ec47548",
+    "url": "/trural/index.html"
+  },
+  {
+    "revision": "c5ff4a836a9cb2b83c4a",
+    "url": "/trural/static/css/2.cf1ed878.chunk.css"
+  },
+  {
+    "revision": "bd248a0e3f0d5c34ccc0",
+    "url": "/trural/static/css/main.596184c9.chunk.css"
+  },
+  {
+    "revision": "c5ff4a836a9cb2b83c4a",
+    "url": "/trural/static/js/2.0327f284.chunk.js"
+  },
+  {
+    "revision": "fdc4023fa9d7d62afa8f74ec645ba6c4",
+    "url": "/trural/static/js/2.0327f284.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd248a0e3f0d5c34ccc0",
+    "url": "/trural/static/js/main.d76ed54a.chunk.js"
+  },
+  {
+    "revision": "03d45733c93f2e85fc7b",
+    "url": "/trural/static/js/runtime-main.104a7b6f.js"
+  },
+  {
+    "revision": "145bf67bc59049251bfe996142c1a39c",
+    "url": "/trural/static/media/g.145bf67b.svg"
+  },
+  {
+    "revision": "f752069aa979db591dd419ed9dcea40b",
+    "url": "/trural/static/media/str.f752069a.svg"
+  },
+  {
+    "revision": "442fa2ec8c0ac7f2e0db0fdde668e6c1",
+    "url": "/trural/static/media/trural.442fa2ec.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.92171f16d7cd6db776b29a12bb5cfb1a.js"
+  "/trural/precache-manifest.a920699e695f555a29e337fd1b31f74b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/trural/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
